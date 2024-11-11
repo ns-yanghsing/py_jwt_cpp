d583d705cc8efc18fa898657ead7ad96189af594
Yanghsing Lin <yanghsingl@netskope.com>
2024-11-11 15:19:09 +0800

    expose encode

--- py_jwt_cpp/__init__.py
+++ py_jwt_cpp/__init__.py
@@ -1,5 +1,14 @@
 from typing import Dict
-from .jwt_cpp import encode_cpp
+from .jwt_cpp import cpp_encode
+
+class InvalidClaimError(Exception):
+    pass
 
 def encode(data: Dict[str, str], private_key: str):
-    return encode_cpp(data, private_key)
+    invalid_keys = [k for k, v in data.items() if not isinstance(v, str)]
+    if invalid_keys:
+        raise InvalidClaimError(
+            f"all values in data should be string. Invalid keys: {invalid_keys}"
+        )
+
+    return cpp_encode(data, private_key)
